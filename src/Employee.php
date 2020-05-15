963e86539e3d31cf4c5707560e01c767a8eaada5
npv34 <phanvanluan611996@gmail.com>
2020-05-15 10:40:56 +0700

    first commit

---
+++ src/Employee.php
@@ -0,0 +1,41 @@
+<?php
+
+
+class Employee
+{
+    protected $id;
+    protected $name;
+    protected $address;
+
+    public function __construct($id, $name, $address)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->address = $address;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+}
